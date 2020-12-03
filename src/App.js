6cc77cd1db53b31ff9b4f1caeef0dbc480d1f3a4
leozaviriukha <leozaviriukha@ukr.net>
2020-12-03 14:49:15 +0200

    add social-layout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <div className='app-wrapper'>
+    <header className='header'>
+      <img src='https://s1.logaster.com/static/v3/img/products/logo.png' />
+    </header>
+    <nav className='nav'>
+      <div>
+        <a>Profile</a>
+      </div>
+      <div>
+        <a>Messages</a>
+      </div>
+      <div>
+        <a>News</a>
+      </div>
+      <div>
+        <a>Music</a>
+      </div>
+      <div>
+        <a>Settings</a>
+      </div>
+    </nav>
+    <div className='content'>
+      <div>
+        <img src='https://ak.picdn.net/shutterstock/videos/6964852/thumb/8.jpg' />
+      </div>
+      <div>
+        ava + descriptions
+      </div>
+      <div>
+        my posts
+        <div>
+          New post
+        </div>
+        <div>
+          <div>
+            post 1
+          </div>
+          <div>
+            post 2
+          </div>
+        </div>
+      </div>
+
     </div>
+  </div>
   );
 }
+
 
 export default App;
